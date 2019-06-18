47dd84b2095994b78e4ea09705659026dd9d1773
adityasagi <aditya.sagi13@gmail.com>
2019-06-18 14:35:40 +0530

    Update setup.sh

--- empty/setup.sh
+++ empty/setup.sh
@@ -2,7 +2,7 @@
 
 # Author: Aditya
 
-cp gen/test_apps.out /lib/firmware/am335x-pru0-fw 
+cp gen/empty.out /lib/firmware/am335x-pru0-fw 
 echo "stop" > /sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a334000.pru/remoteproc/remoteproc1/state
 echo "start" > /sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a334000.pru/remoteproc/remoteproc1/state
 
